5b6f4783ccc3990efb142c15fd48847180add28a
Alex-Diez <alex.dukhno@icloud.com>
2017-11-19 18:49:22 +0200

    MVP of the target sum game

--- src/components/TargetSumGame.js
+++ src/components/TargetSumGame.js
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class TargetSumGame extends React.Component {
+export default class TargetSumGame extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+    this.state = {
+      selectedNumbers: [],
+      points: 0
+    };
+
+    this.generate = this.generate.bind(this);
+    this.checkNumber = this.checkNumber.bind(this);
+  }
+
+  generate(e) {
+    e.preventDefault();
+    this.props.onRestart();
+    const numbers = this.props.numberSource();
+    const target = this.props.targetComputer(numbers);
+    this.setState({target: target, numbers: numbers, points: 0, selectedNumbers: []});
+  }
+
+  checkNumber(index, value) {
+    this.setState(
+      (prevState, props) => ({
+        points: prevState.points + value,
+        selectedNumbers: prevState.selectedNumbers.concat(index)
+      })
+    );
+  }
+
   render() {
-    return <div/>
+
+    const createNumberNode = (index, number) => {
+      return (
+        <Number
+          key={index}
+          value={number}
+          clickable={this.state.selectedNumbers.indexOf(index) === -1}
+          onCheck={() => this.checkNumber(index, number)}
+        />
+      )
+    };
+
+    const target = this.state.target === undefined ? '?' : this.state.target;
+    const numbers = this.state.numbers === undefined ? new Array(6).fill('?') : this.state.numbers;
+
+    return (
+      <div className='game'>
+        <TargetSum target={target} points={this.state.points}/>
+        <div className='challenge-numbers'>
+          {numbers.map((num, index) => createNumberNode(index, num))}
+        </div>
+        <button
+          className='start'
+          onClick={this.generate}
+        >
+          start the game
+        </button>
+      </div>
+    )
+  }
+}
+
+class Number extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+
+    this.checkIn = this.checkIn.bind(this);
+  }
+
+  checkIn(e) {
+    e.preventDefault();
+
+    if (this.props.clickable) {
+      this.props.onCheck();
+    }
+  }
+
+  render() {
+    const classes = this.props.clickable ? 'number clickable' : 'number clicked';
+
+    return (
+      <div
+        className={classes.toString()}
+        onClick={this.checkIn}
+      >
+        {this.props.value}
+      </div>
+    )
+  }
+}
+
+class TargetSum extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+  }
+
+  render() {
+    let stateClass;
+    if (this.props.target === this.props.points) {
+      stateClass = 'target success';
+    } else if (this.props.target <= this.props.points) {
+      stateClass = 'target failure';
+    } else {
+      stateClass = 'target';
+    }
+    return <div className={stateClass.toString()}>{this.props.target}</div>;
   }
 }
--- src/components/__tests__/TargetSumGame.test.js
+++ src/components/__tests__/TargetSumGame.test.js
@@ -1,3 +1,4 @@
+import 'jsdom-global/register';
 import React from 'react';
 import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -7,15 +8,99 @@ import TargetSumGame from '../TargetSumGame';
 configure({adapter: new Adapter()});
 
 describe('target sum game', () => {
+  let game;
+  let target;
+  let numbers;
+  let start;
+
+  beforeEach(() => {
+    const dummyGenerator = () => [1, 2, 3, 4, 5, 6];
+
+    game = mount(
+      <TargetSumGame
+        gameId={1}
+        targetComputer={(numbers) => 12}
+        numberSource={dummyGenerator}
+        onRestart={() => {}}
+      />
+    );
+
+    target = game.find('.target');
+    numbers = game.find('.number');
+    start = game.find('.start');
+  });
+
+  const startGame = () => start.simulate('click');
+
+  const contentOf = (nodes) => nodes.map(node => node.text());
+
   test('check initial state of the game', () => {
-    const game = mount(<TargetSumGame/>);
-
-    const target = game.find('.target');
-    const numbers = game.children('.number');
-    const start = game.find('.start');
-
     expect(target.text()).toBe('?');
-    expect(numbers.map(item => item.text())).toBeEqual(['?', '?', '?', '?', '?', '?']);
+    expect(numbers.map(num => num.text())).toEqual(['?', '?', '?', '?', '?', '?']);
     expect(start.text()).toBe('start the game');
+  });
+
+  test('target and numbers should be generated when start button is clicked', () => {
+    startGame();
+
+    expect(target.text()).toBe('12');
+    expect(contentOf(numbers)).toEqual(['1', '2', '3', '4', '5', '6']);
+  });
+
+  test('number button becomes clicked and can\'t be clicked again', () => {
+    startGame();
+
+    numbers.at(0).simulate('click');
+
+    let clickedNumbers = game.find('.clicked');
+
+    expect(contentOf(clickedNumbers)).toContain('1');
+  });
+
+  test('target must have green color when player succeeds', () => {
+    startGame();
+
+    numbers.at(0).simulate('click');
+    numbers.at(4).simulate('click');
+    numbers.at(5).simulate('click');
+
+    expect(game.find('.success').exists()).toBe(true);
+  });
+
+  test('target must have red color when player failed', () => {
+    startGame();
+
+    numbers.at(0).simulate('click');
+    numbers.at(1).simulate('click');
+    numbers.at(3).simulate('click');
+    numbers.at(5).simulate('click');
+
+    expect(game.find('.failure').exists()).toBe(true);
+  });
+
+  test('number can be clicked only once', () => {
+    startGame();
+
+    numbers.at(0).simulate('click');
+    numbers.at(4).simulate('click');
+    numbers.at(4).simulate('click');
+    numbers.at(4).simulate('click');
+    numbers.at(5).simulate('click');
+
+    expect(game.find('.success').exists()).toBe(true);
+  });
+
+  test('game should be restarted when "start game" clicked', () => {
+    startGame();
+
+    numbers.at(0).simulate('click');
+    numbers.at(4).simulate('click');
+
+    startGame();
+
+    let newNumbers = game.find('.number');
+    newNumbers.at(5).simulate('click');
+
+    expect(game.find('.success').exists()).toBe(false);
   });
 });
